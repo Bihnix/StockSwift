bb96d54cd08f2ebf0b5b7f20081b55efbe8b23f2
Behnam <Do you really need my email>
2024-04-30 21:46:46 +0100

    Adding swift code

---
+++ StockSwift/StockSwift/StockSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSwiftApp.swift
+//  StockSwift
+//
+//  Created by Behnam Ebrahimi on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct StockSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
